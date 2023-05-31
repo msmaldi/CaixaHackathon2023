33f85be562e9a796207fe184bf1ddb0681ca52b3
Matheus Maldi <msmaldi@hotmail.com>
2023-05-31 18:50:42 -0300

    Fix Format

--- src/Caixa.Hackathon2023/Calculadora/Calculadora.cs
+++ src/Caixa.Hackathon2023/Calculadora/Calculadora.cs
@@ -1,59 +1,58 @@
+namespace Caixa.Hackathon2023;
+
 using Caixa.Hackathon2023.Models;
 
-namespace Caixa.Hackathon2023
+public class Calculadora
 {
-    public class Calculadora
+    public double ValorPresente { get; private set; }
+    public double TaxaJuros { get; private set; }
+    public double Meses { get; private set; }
+    public Calculadora(double valorPresente, double taxaJuros, int meses)
     {
-        public double ValorPresente { get; private set; }
-        public double TaxaJuros { get; private set; }
-        public double Meses { get; private set; }
-        public Calculadora(double valorPresente, double taxaJuros, int meses)
+        ValorPresente = valorPresente;
+        TaxaJuros = taxaJuros;
+        Meses = meses;
+    }
+
+    public List<ParcelaDTO> GerarSac()
+    {
+        int numero = 1;
+        double valorAmortizacao = ValorPresente / Meses;
+        double saldoDevedor = ValorPresente;
+
+        var parcelas = new List<ParcelaDTO>();
+
+        for (int i = 0; i < Meses; i++, numero++)
         {
-            ValorPresente = valorPresente;
-            TaxaJuros = taxaJuros;
-            Meses = meses;
+            double valorJuros = saldoDevedor * TaxaJuros;
+            double valorPrestacao = valorJuros + valorAmortizacao;
+            saldoDevedor -= valorAmortizacao;
+
+            var parcela = new ParcelaDTO(numero, valorAmortizacao, valorJuros, valorPrestacao);
+            parcelas.Add(parcela);
         }
 
-        public List<ParcelaDTO> GerarSac()
+        return parcelas;
+    }
+
+    public List<ParcelaDTO> GerarPrice()
+    {
+        var parcelas = new List<ParcelaDTO>();
+
+        int numero = 1;
+        double valorPrestacao = (ValorPresente * TaxaJuros) / (1 - (1 / Math.Pow(1 + TaxaJuros, Meses)));
+        double saldoDevedor = ValorPresente;
+
+        for (int i = 0; i < Meses; i++, numero++)
         {
-            int numero = 1;
-            double valorAmortizacao = ValorPresente / Meses;
-            double saldoDevedor = ValorPresente;
+            double valorJuros = saldoDevedor * TaxaJuros;
+            double valorAmortizacao = valorPrestacao - valorJuros;
+            saldoDevedor -= valorAmortizacao;
 
-            var parcelas = new List<ParcelaDTO>();
-
-            for (int i = 0; i < Meses; i++, numero++)
-            {
-                double valorJuros = saldoDevedor * TaxaJuros;
-                double valorPrestacao = valorJuros + valorAmortizacao;
-                saldoDevedor -= valorAmortizacao;
-
-                var parcela = new ParcelaDTO(numero, valorAmortizacao, valorJuros, valorPrestacao);
-                parcelas.Add(parcela);
-            }
-
-            return parcelas;
+            var parcela = new ParcelaDTO(numero, valorAmortizacao, valorJuros, valorPrestacao);
+            parcelas.Add(parcela);
         }
 
-        public List<ParcelaDTO> GerarPrice()
-        {
-            var parcelas = new List<ParcelaDTO>();
-
-            int numero = 1;
-            double valorPrestacao = (ValorPresente * TaxaJuros) / (1 - (1 / Math.Pow(1 + TaxaJuros, Meses)));
-            double saldoDevedor = ValorPresente;
-
-            for (int i = 0; i < Meses; i++, numero++)
-            {
-                double valorJuros = saldoDevedor * TaxaJuros;
-                double valorAmortizacao = valorPrestacao - valorJuros;
-                saldoDevedor -= valorAmortizacao;
-
-                var parcela = new ParcelaDTO(numero, valorAmortizacao, valorJuros, valorPrestacao);
-                parcelas.Add(parcela);
-            }
-
-            return parcelas;
-        }
+        return parcelas;
     }
 }
